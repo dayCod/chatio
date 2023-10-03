87835b500988ac92b51b31071e9d860ca3356917
dayCod <alayarahman07@gmail.com>
2023-10-03 14:08:00 +0700

    Update: Chatio Chatroom Users

--- app/Livewire/Chatio.php
+++ app/Livewire/Chatio.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Chatio extends Component
 {
+    /**
+     * set public user variable.
+     */
+    public $users;
+
+    /**
+     * set mounted functions.
+     */
+    public function mount()
+    {
+        $this->users = User::orderBy('created_at', 'DESC')->take(5)->get();
+    }
+
+    /**
+     * render chatio view.
+     */
     public function render()
     {
         return view('livewire.chatio');
