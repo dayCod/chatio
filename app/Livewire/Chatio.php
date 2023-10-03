fef65e6afc099205d4046927eaa1c9a2ac09c9b3
dayCod <alayarahman07@gmail.com>
2023-10-03 11:44:54 +0700

    Add: Login, Register, Chat Room View

---
+++ app/Livewire/Chatio.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Chatio extends Component
+{
+    public function render()
+    {
+        return view('livewire.chatio');
+    }
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Chatio Login</title>
+</head>
+
+<body>
+
+    <div class="container">
+      <div class="row align-items-center justify-content-center" style="height: 100vh">
+        <div class="col-10">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-3">Chatio Login</h5>
+              <form action="">
+                <div class="mb-3">
+                  <label class="form-label">Email address</label>
+                  <input type="email" class="form-control" placeholder="name@example.com" required>
+                </div>
+                <div class="mb-3">
+                  <label class="form-label">Password</label>
+                  <input type="password" class="form-control" placeholder="********" required>
+                </div>
+                <div class="mb-3">
+                  <div class="d-grid">
+                    <button type="submit" class="btn btn-success">Login</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
